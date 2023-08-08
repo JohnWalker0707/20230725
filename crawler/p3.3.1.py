145d26a20022bede7707871fccb0050cc181f069
JohnWalker0707 <140509654+JohnWalker0707@users.noreply.github.com>
2023-08-08 13:59:20 +0000

    88

--- crawler/p3.3.1.py
+++ crawler/p3.3.1.py
@@ -13,12 +13,22 @@ def got(url):
     return BeautifulSoup(respon,'lxml')
 
 soup=got(url)
-url2=soup.find('a',class_='menu__item-link')['href']
+url2=soup.find_all('a',class_='menu__item-link')[1]['href']
+
 soup2=got(url2)
 doc=soup2.find('a',class_='qa-list__title-link')
-print(doc['href'])
 url3=doc['href']
+print(url2+'  >>>  '+url3+'\n')
+doc_text=got(url3).find('div',class_="leftside")
 
+print('作者: '+doc_text.find('a',class_='qa-header__info-person').get_text(strip=True))
+print('發文時間: '+doc_text.find('a',class_='qa-header__info-time').get_text(strip=True))
+tag=''
+for i in doc_text.find_all('a',class_='tag qa-header__tagList'):
+    tag=tag+' '+i.get_text(strip=True)
+print('文章標籤:'+tag)
+print('瀏覽數: '+doc_text.find('span',class_='qa-header__info-view').get_text(strip=True).replace('瀏覽',""))
+# print(doc_text.get_text(strip=True))
 
 
 
